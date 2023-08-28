c8d3c02eba702ac7220b4f4610020eed09e7bf9d
AdityaBhadoriya123 <143399571+AdityaBhadoriya123@users.noreply.github.com>
2023-08-28 12:50:34 +0000

    for datatype and variables

---
+++ 01_basics/variables.js
@@ -0,0 +1,15 @@
+const account_Id = 1001
+
+let acc_email = "john_doe24@gmail.com"
+
+var pswd = "12345"
+city = "delhi"
+//account_Id=89
+
+console.log(account_Id);
+
+acc_email = "shenyl@gmail.com"
+pswd="9764"
+city="banglore"
+
+console.table([account_Id,acc_email,pswd,city])
