ccde517b1016de4f347f27eb7c7e4eaf1cac3db4
AdityaBhadoriya123 <143399571+AdityaBhadoriya123@users.noreply.github.com>
2023-08-29 11:58:33 +0000

    Conversion of datatypes

---
+++ 01_basics/conversion_basics.js
@@ -0,0 +1,22 @@
+// let score = false;
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let ValueInNumber = Number(score);
+
+// console.log(ValueInNumber);
+
+// //"33" this can string can be converted to a Number but "33abc" cannot be a number-NAN
+// //true->1 false->0
+
+let isLoggedIn = 1
+let booleanLogIn = Boolean(isLoggedIn);
+
+console.log(booleanLogIn);
+
+let someNum=33
+let stringNum=String(someNum)
+
+console.log(stringNum);
+console.log(typeof stringNum);
--- 01_basics/datatypes.js
+++ 01_basics/datatypes.js
@@ -1,3 +1,3 @@
 "use strict";//treat all JS code as per new standards
 
-alert("hello")
+// alert("hello") // we are using node
